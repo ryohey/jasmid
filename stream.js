d5d76fda653885a93046b1295d97dbfebc27985a
ryohey <info@codingcafe.jp>
2016-09-26 20:47:31 +0900

    remove semicolons

--- stream.js
+++ stream.js
@@ -1,11 +1,11 @@
 /* Wrapper for accessing strings through sequential reads */
 function Stream(str) {
-  var position = 0;
+  var position = 0
 
   function read(length) {
-    var result = str.substr(position, length);
-    position += length;
-    return result;
+    var result = str.substr(position, length)
+    position += length
+    return result
   }
 
   /* read a big-endian 32-bit integer */
@@ -14,30 +14,30 @@ function Stream(str) {
       (str.charCodeAt(position) << 24)
       + (str.charCodeAt(position + 1) << 16)
       + (str.charCodeAt(position + 2) << 8)
-      + str.charCodeAt(position + 3));
-    position += 4;
-    return result;
+      + str.charCodeAt(position + 3))
+    position += 4
+    return result
   }
 
   /* read a big-endian 16-bit integer */
   function readInt16() {
     var result = (
       (str.charCodeAt(position) << 8)
-      + str.charCodeAt(position + 1));
-    position += 2;
-    return result;
+      + str.charCodeAt(position + 1))
+    position += 2
+    return result
   }
 
   /* read an 8-bit integer */
   function readInt8(signed) {
-    var result = str.charCodeAt(position);
-    if (signed && result > 127) result -= 256;
-    position += 1;
-    return result;
+    var result = str.charCodeAt(position)
+    if (signed && result > 127) result -= 256
+    position += 1
+    return result
   }
 
   function eof() {
-    return position >= str.length;
+    return position >= str.length
   }
 
   /* read a MIDI-style variable-length integer
@@ -45,15 +45,15 @@ function Stream(str) {
     with top bit set to signify that another byte follows)
   */
   function readVarInt() {
-    var result = 0;
+    var result = 0
     while (true) {
-      var b = readInt8();
+      var b = readInt8()
       if (b & 0x80) {
-        result += (b & 0x7f);
-        result <<= 7;
+        result += (b & 0x7f)
+        result <<= 7
       } else {
         /* b is the last byte */
-        return result + b;
+        return result + b
       }
     }
   }
